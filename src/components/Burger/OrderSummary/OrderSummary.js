1b0298a7bb34c6fa284cbfa1ff87874ae1169158
GabrielVaradi <gabriel.varadi.hallope.lecoq@gmail.com>
2021-03-10 08:06:57 -0500

    add logic and style to modal

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -2,7 +2,21 @@ import React from "react"
 
 
 const orderSummary = (props) => {
-
+    const ingredientSummary = Object.keys(props.ingredients).map((ingredientKey => {
+        return  <li> 
+                    <span style={{ textTransform: "capitalize" }}> {ingredientKey} </span> : {props.ingredients[ingredientKey]}
+                </li>
+    }))
+    return (
+        <React.Fragment>
+            <h3> Your Order </h3>
+            <p> With these ingredients : </p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p> Continue to checkout? </p>
+        </React.Fragment>
+    )
 }
 
 export default orderSummary
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,10 +1,9 @@
 import React from 'react'
 
-const modal = (props) => {
+const modal = (props) => (
     <div className={"modal"}>
         {props.children}
     </div>
-
-}
+)
 
 export default modal
